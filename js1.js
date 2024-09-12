ac8516038ea33611225548f4cf74b47c8955b2ae
Pradyumna <bhardwaj.pradyumn.pb@gmail.com>
2024-09-12 15:20:22 +0530

    Js file updated

--- js1.js
+++ js1.js
@@ -1,40 +1,19 @@
-const url =
-    'https://api.openweathermap.org/data/2.5/weather';
-const apiKey =
-    'eb299c39349839671d39ecd5ee774bc4';
+const apiKey = "eb299c39349839671d39ecd5ee774bc4";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?"
 
-
-
-async function weatherFn(cName) {
-    const temp =
-        `${url}?q=${cName}&appid=${apiKey}&units=metric`;
-    try {
-        const res = await fetch(temp);
-        const data = await res.json();
-        if (res.ok) {
-            weatherShowFn(data);
-        } else {
-            alert('City not found. Please try again.');
-        }
-    } catch (error) {
-        console.error('Error fetching weather data:', error);
-    }
+    
+async function checkWeather(city) {
+const response = await fetch(`${apiUrl}units=metric&q=${city}` + `&appid=${apiKey}`);
+var data = await response.json();
+console.log(data);
+showWeather(data);
 }
 
-function weatherShowFn(data) {
-    $('#city-name').text(data.name);
-    $('#date').text(moment().
-        format('MMMM Do YYYY, h:mm:ss a'));
-    $('#temperature').
-        html(`${data.main.temp}°C`);
-    $('#description').
-        text(data.weather[0].description);
-    $('#wind-speed').
-        html(`Wind Speed: ${data.wind.speed} m/s`);
-    $('#weather-icon').
-        attr('src',
-            `...`);
-    $('#weather-info').fadeIn();
+function showWeather(data){
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 }
 
 weatherFn('Pune');
